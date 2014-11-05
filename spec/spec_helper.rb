64c2772f1cbf67afc566a8f1a31bdca974abee2c
Manmeet Saluja <manmeet.saluja94@gmail.com>
2014-11-05 12:04:46 -0800

    updated home page spec and spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,11 +4,15 @@ require 'capybara/dsl'
 
 Capybara.configure do |config|
   config.current_driver = :selenium
-  config.run_server = false
+  config.run_server = true
   config.app_host   = 'http://localhost:3000'
 end
 
 RSpec.configure do |config|
   config.include Capybara::DSL
   config.order = :random
+  config.expose_current_running_example_as :example
 end
+
+
+
